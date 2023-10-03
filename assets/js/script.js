708b958dff3c28d3c9da1c66035bd00788bfb32e
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-10-03 17:48:57 +0200

    display select placeholder + CodeQL + ESLint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1242,7 +1242,8 @@ addEventListener("DOMContentLoaded", () => {
 							buttonElement.classList.add("select")
 
 							const svgSelect = document.createElement("div")
-							svgSelect.innerHTML = "<svg aria-hidden='true' role='img' width='24' height='24' viewBox='0 0 24 24'><path fill='currentColor' d='M16.59 8.59003L12 13.17L7.41 8.59003L6 10L12 16L18 10L16.59 8.59003Z'></path></svg>"
+							svgSelect.innerHTML = "<svg aria-hidden='true' role='img' width='24' height='24' viewBox='0 0 24 24'><path fill='currentColor' d='M16.59 8.59003L12 13.17L7.41 8.59003L6 " +
+								"10L12 16L18 10L16.59 8.59003Z'></path></svg>" + component.placeholder
 							buttonElement.appendChild(svgSelect)
 						}
 
