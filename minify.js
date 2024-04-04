81ca3cfb131ef2429a29292b991b343be4ac3b14
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-04-04 18:02:54 +0200

    Restore previous style.css for now

--- minify.js
+++ minify.js
@@ -123,7 +123,7 @@ async function main() {
 		}
 	})
 
-	//await minifyFile("./assets/style.css")
+	await minifyFile("./assets/style.css")
 	await minifyFile("./assets/manifest.json")
 
 	results.push({
@@ -134,4 +134,4 @@ async function main() {
 	})
 	console.table(results.sort((a, b) => a["% reduction"] - b["% reduction"]))
 }
-//main()
+main()
